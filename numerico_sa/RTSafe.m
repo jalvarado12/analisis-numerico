fe77accba8fc679b716f33193aed62cc344f7f66
jalvarado12 <105568949+jalvarado12@users.noreply.github.com>
2025-08-06 15:27:45 -0500

    Update RTSafe.m

--- numerico_sa/RTSafe.m
+++ numerico_sa/RTSafe.m
@@ -1,8 +1,10 @@
 function sal = RTSafe(f, df, x1, x2, tol, N0)
     fx1 = f(x1);
     fx2 = f(x2);
+   
     if fx1 * fx2 > 0
-        fprintf('Error: La ra´ız no est´a contenida en el intervalo');
+        fprintf('Error: La raíz no está contenida en el intervalo\n');
+        return
     end
     if abs(fx1) < tol
         sal = x1;
@@ -16,31 +18,34 @@ function sal = RTSafe(f, df, x1, x2, tol, N0)
     xh = x2;
 
     rts = (x1 + x2)/2;
-    dx = abs(x2-x1);
+    dx = abs(x2 - x1);
 
-    frts = f(rts);
-    dfrts = df(rts);
+    i = 1;
+    while i <= N0
+        frts = f(rts);    
+        dfrts = df(rts);     
 
-    while i<= N0
         if abs(dfrts) <= tol || rts < xl || rts > xh
-            dx = (xh-xl)/2;
+            dx = (xh - xl)/2;
             rts = xl + dx;
         else 
-            dx = frts/ dfrts;
+            dx = frts / dfrts;
             rts = rts - dx;
-      
         end
+
         if abs(dx) < tol
             sal = rts;
             return
         end
+        
         if f(rts) < 0
             xl = rts;
         else
             xh = rts;
         end
-        i = i +1;
-    end
-    fprintf('Method failed after %d iterations', N0);
-end
 
+        i = i + 1;
+    end
+
+    fprintf('Method failed after %d iterations\n', N0);
+end
