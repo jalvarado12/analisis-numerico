76016b1786aa7a710a2cee7510d17d108541f319
Sebastian Alvarado <105568949+jalvarado12@users.noreply.github.com>
2025-08-07 15:21:14 -0500

    Update secante.m

--- numerico_sa/secante.m
+++ numerico_sa/secante.m
@@ -2,17 +2,22 @@ function p = secante(f, p0, p1, tol, N0)
     i = 2;
     q0 = f(p0);
     q1 = f(p1);
-    while i<= N0
-        p = p1 - q1 * ((p1 - p0)/(q1 - q0));
+
+    while i <= N0
+        p = p1 - q1 * ((p1 - p0)/(q1 - q0));  % Fórmula de la secante
+
         if abs(p - p1) < tol
-            fprintf('Se encontro solución despues de %d iteraciones', i);
+            fprintf('Número de iteraciones: %d\n', i);
             return 
         end 
-        i = i +1;
-        p0 = q1;
+
+        % Actualizamos valores para la siguiente iteración
+        i = i + 1;
+        p0 = p1;
         q0 = q1;
         p1 = p;
         q1 = f(p);
     end
-    fprintf('El metodo fallo despues de %d iteraciones', i);
+
+    fprintf('El método falló después de %d iteraciones\n', i);
 end
