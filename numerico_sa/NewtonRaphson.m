564d418143dcfe4e885da6fc52e8b84562b4b537
Sebastian Alvarado <105568949+jalvarado12@users.noreply.github.com>
2025-08-07 15:18:17 -0500

    Update NewtonRaphson.m

--- numerico_sa/NewtonRaphson.m
+++ numerico_sa/NewtonRaphson.m
@@ -1,14 +1,20 @@
 function p_new = NewtonRaphson(f, df, p0, tol, N0)
     i = 1;
     p = p0;
+
     while i <= N0
+        % Aplicamos la fórmula de Newton-Raphson
         p_new = p - (f(p)/df(p));
+
+        % Si la diferencia entre iteraciones es menor que la tolerancia, paramos
         if abs(p_new - p) < tol
-            fprintf('Numero de iteraciones: %d', i);
+            fprintf('Numero de iteraciones: %d\n', i);
             return
         end
-        i = i +1;
-        p = p_new;
+
+        i = i + 1;
+        p = p_new;  % Actualizamos p para la siguiente iteración
     end
-    fprintf('Method failed after %d iterations', N0);
-end    
+
+    fprintf('Method failed after %d iterations\n', N0);
+end
