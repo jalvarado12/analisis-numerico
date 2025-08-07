683268b3eccdbb42741d304cb7a10fc034dbc198
Sebastian Alvarado <105568949+jalvarado12@users.noreply.github.com>
2025-08-07 15:19:21 -0500

    Update bisectionMethod.m

--- numerico_sa/bisectionMethod.m
+++ numerico_sa/bisectionMethod.m
@@ -1,18 +1,20 @@
 function p = bisectionMethod(f, a, b, tol, N0)
    i = 1;
-   FA = f(a);
-
+   FA = f(a);  % Evaluamos f en el extremo izquierdo del intervalo
 
    while i <= N0
+       p = a + (b - a)/2;   % Calculamos el punto medio del intervalo
+       FP = f(p);           % Evaluamos f en el punto medio
 
-       p = a+ (b-a)/2;
-       %fprintf(p);
-       FP = f(p);
-       if FP == 0 || (b-a)/2 < tol
-           fprintf('Numero de iteraciones: %d', i);
+       % Si f(p) es cero o el tamaño del intervalo es menor que la tolerancia, terminamos
+       if FP == 0 || (b - a)/2 < tol
+           fprintf('Numero de iteraciones: %d\n', i);
            return
        end
-       i = i+1;
+
+       i = i + 1;  % Aumentamos el contador de iteraciones
+
+       % Verificamos en qué subintervalo se mantiene el cambio de signo
        if FA * FP > 0
            a = p;
            FA = FP;
@@ -20,5 +22,7 @@ function p = bisectionMethod(f, a, b, tol, N0)
            b = p;
        end
    end    
-   fprintf('Method failed after %d iterations', N0);
-end    
+
+   % Si llegamos aquí, el método falló en encontrar la raíz con la precisión requerida
+   fprintf('Method failed after %d iterations\n', N0);
+end
